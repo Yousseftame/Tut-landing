d4d010ede103033b29e0b8c78acdfc4647b4e673
Yousseftame <tamerosama73@gmail.com>
2026-04-06 15:33:20 +0200

    fix: case sensitive import

--- src/layouts/AdminLayOut/AdminLayOut.tsx
+++ src/layouts/AdminLayOut/AdminLayOut.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../../components/Sidebar/Sidebar";
-import AdminNavbar from "@/components/AdminNavbar/adminNavbar";
+import AdminNavbar from "@/components/AdminNavbar/AdminNavbar";
 
 export default function AdminLayout() {
   return (
